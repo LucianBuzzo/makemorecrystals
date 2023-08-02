f3022da05854ad917d6290dc36682ae80f3f4674
Lucian Buzzo <lucian.buzzo@gmail.com>
2023-08-02 08:50:50 +0100

    ensure probabilities are duplicated not reference

--- nn.cr
+++ nn.cr
@@ -2,7 +2,14 @@ require "num"
 require "./multinomial"
 
 Num::Rand.set_seed(2147483647)
-ctx = Num::Grad::Context(Tensor(Float64, CPU(Float64))).new
+
+def one_hot(input, num_classes)
+  out = Tensor(Float64, CPU(Float64)).zeros([input.size, num_classes])
+  input.each_with_index do |v, i|
+    out[i, v] = 1
+  end
+  out
+end
 
 # Define an array to hold the lines
 lines = [] of String
@@ -32,7 +39,7 @@ end
 xs = [] of Int32
 ys = [] of Int32
 
-lines[0...1].each do |word|
+lines.each do |word|
   chs = [".", *word.chars, "."]
   chs.each_cons_pair do |ch1, ch2|
     bigram = {ch1.to_s, ch2.to_s}
@@ -46,36 +53,60 @@ end
 xs = xs.to_tensor
 ys = ys.to_tensor
 
-def one_hot(input, num_classes)
-  out = Tensor(Float64, CPU(Float64)).zeros([input.size, num_classes])
-  input.each_with_index do |v, i|
-    out[i, v] = 1
+puts "number of samples: #{xs.size}"
+
+xenc = one_hot(xs, 27)
+
+w = Tensor(Float64, CPU(Float64)).normal([27, 27])
+
+probs = Tensor(Float64, CPU(Float64)).zeros([27, 27])
+
+50.times do |epoch|
+  # Forward pass
+  logits = xenc.matmul(w)
+  # softmax
+  counts = logits.exp
+  probs = counts / counts.sum(axis: 1, dims: true)
+
+  loss = [] of Float64
+  probs.shape[0].times { |k|
+    loss << probs[k, ys[k].value].first
+  }
+
+  loss = 0 - loss.to_tensor.log.mean + 0.01 * (w**2).mean
+
+  puts "loss #{loss}"
+
+  # Calculate the softmax gradients
+  dprobs = probs.dup
+
+  dprobs.shape[0].times do |i|
+    dprobs[i, ys[i].value] -= 1.0
   end
-  out
+
+  dprobs /= ys.size
+
+  # Now calculate the gradients for weight w
+  dw = xenc.transpose.matmul(dprobs)
+
+  # Set a learning rate
+  learning_rate = 50
+
+  # Update the weights using gradient descent
+  w -= dw * learning_rate
 end
 
-xenc = ctx.variable(one_hot(xs, 27))
+10.times do
+  outs = [] of (String | Char)
 
-w = ctx.variable(Tensor(Float64, CPU(Float64)).normal([27, 27]))
-puts w
+  ix = 0
+  while true
+    p = probs[ix]
+    ix = Multinomial.sample(p.to_a, 1).first
+    outs << itos[ix]
 
-net = Num::NN::Network.new(ctx)
+    break if ix == 0
+  end
 
-logits = xenc.matmul(w)
-# softmax
-counts = logits.exp
-puts counts
-probs = counts / ctx.variable(counts.value.sum(axis: 1, dims: true))
-
-puts probs[0]
-puts ys
-
-puts Tensor.range(5)
-loss = [] of Float64
-5.times { |k|
-  loss << probs[k, ys.to_a[k]].value.first
-}
-
-loss = 0 - loss.to_tensor.log.mean
-
-puts loss
+  puts outs.join
+end
